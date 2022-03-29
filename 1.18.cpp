24d8e6b6721aedfe4af7f443f64696eecd03147e
0dxyfd0 <100783150+0dxyfd0@users.noreply.github.com>
2022-03-29 21:20:34 +0800

    Add files via upload

---
+++ 1.18.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{int k=1;
+	cout<<"N"<<" "<<"10*N"<<" "<<"100*N"<<" "<<"1000*N"<<endl;
+	while(k<=5)
+	{
+		cout<<k<<"  "<<k*10<<"  "<<k*100<<"   "<<k*1000<<endl; 
+		k++;
+	}
+	cout<<"\t";
+ } 
---
+++ 2.16.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{int a,b;
+    cout<<"please enter two integers";
+    cin>>a>>b;
+    cout<<"ºÍ"<<"="<<a+b<<endl;
+    cout<<"³Ë»ý"<<"="<<a*b<<endl;
+    cout<<"²î"<<"="<< a-b<<endl;
+	cout<<"ÉÌ"<<"="<<a/b<<endl;    
+    return 0;
+    
+ } 
